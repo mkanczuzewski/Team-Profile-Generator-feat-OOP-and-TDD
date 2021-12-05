737de5c5d0d094803b32dd6a0e254d4b452d4321
mkanczuzewski <mkanczuzewski@gmail.com>
2021-12-05 16:57:10 -0500

    Add in manager objects and testing

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require('../lib/Manager');
+
+test('creates an manager object', () => {
+    const manager = new Manager('Mark', 101, 'mark@fakeEmail.com', 5002);
+
+    expect(manager.name).toBe('Mark');
+    expect(manager.id).toBe(101);
+    expect(manager.emailAddress).toBe('mark@fakeEmail.com');
+    expect(manager.officeNo).toBe(5002);
+});
+
+test('creates an manager object and verifies getRole', () => {
+    const manager = new Manager('Mark', 101, 'mark@fakeEmail.com');
+
+    expect(manager.getRole()).toBe('Manager');
+});
